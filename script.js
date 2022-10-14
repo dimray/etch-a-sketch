c778b092ef61d1574d756f4d06e7277a15c632f8
dimray <dimray@gmail.com>
2022-10-14 17:11:46 +0100

    Change grid to 16 x 16

--- script.js
+++ script.js
@@ -4,16 +4,18 @@ const container = document.querySelector("div.container");
 const box = document.createElement("div");
 box.classList = ".box";
 box.style.backgroundColor = "white";
-box.style.height = "70px";
-box.style.width = "70px"
+box.style.height = "43px";
+box.style.width = "43px"
 box.style.border = "1px solid black";
 box.style.margin = "0";
 box.style.padding = "0";
 
 
-for (let i = 0; i < 100; i++) {
+for (let i = 0; i < 256; i++) {
     container.appendChild(box.cloneNode(true));
 }
+
+
 
 
 
