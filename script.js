d50ef7f8d273ec1083ea81ccc28b569c3a49ec41
dimray <dimray@gmail.com>
2022-10-15 16:01:23 +0100

    functioning grids and size selector

--- script.js
+++ script.js
@@ -1,26 +1,45 @@
-const container = document.querySelector("div.container");
+const container = document.querySelector(".container");
+
+const grid = document.querySelector("#squares");
+const btn = document.querySelector("#btn");
+
+btn.addEventListener("click", function (event) {
+    event.preventDefault();
+    setGrid(+ grid.value);
+    console.log(grid.value);
+})
+
+gridValue = + grid.value;
 
 
-const box = document.createElement("div");
-box.classList = ".box";
-box.style.backgroundColor = "white";
-box.style.height = "43px";
-box.style.width = "43px"
-box.style.border = "1px solid black";
-box.style.margin = "0";
-box.style.padding = "0";
+function setGrid(gridValue) {
 
 
-for (let i = 0; i < 256; i++) {
-    container.appendChild(box.cloneNode(true));
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    };
+
+    for (let i = 0; i < (gridValue * gridValue); i++) {
+
+        const box = document.createElement("div");
+        box.classList.add("box");
+        box.style.height = 720 / gridValue - 2 + "px";
+        box.style.width = 720 / gridValue - 2 + "px";
+        box.style.border = "1px solid black";
+        box.style.margin = "0";
+        box.style.padding = "0";
+        container.appendChild(box);
+    }
+
+    const boxes = document.querySelectorAll(".box");
+
+    boxes.forEach(function (box) {
+        box.addEventListener("mouseover", function () {
+            box.classList.add("pink");
+        });
+    });
+
 }
-
-
-
-
-
-
-
 
 
 
